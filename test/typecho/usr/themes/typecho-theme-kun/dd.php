ed1e85a34207f82d878d1c59877e07af65a6722a
mozzie <himozzie@foxmail.com>
2024-06-06 09:04:23 +0800

    chore: remove typecho cache file

--- test/typecho/usr/themes/typecho-theme-kun/dd.php
+++
@@ -1,3 +0,0 @@
-<script src="<?php $this->options->themeUrl('6190371e.js'); ?>" ></script>
-
-<div>ddd</div>
--- test/typecho/usr/themes/typecho-theme-kun/header.php
+++
@@ -1,3 +0,0 @@
-<script src="<?php $this->options->themeUrl('cef695b6.js'); ?>" ></script>
-
-<header>头部</header>
