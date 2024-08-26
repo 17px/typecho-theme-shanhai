4458d333b42f3bc8bd26f89820294dc41a10b8a8
mozzie <himozzie@foxmail.com>
2024-08-26 11:38:57 +0800

    feat: inline katex

--- apps/kun/src/modules/post/index.php
+++ apps/kun/src/modules/post/index.php
@@ -174,7 +174,24 @@
 
     <?php if (in_array('UseKatex', $this->options->moreConfig)) : ?>
         <script defer src="<?php $this->options->themeUrl('assets/katex/katex.min.js'); ?>"></script>
-        <script defer src="<?php $this->options->themeUrl('assets/katex/auto.render.min.js'); ?>" onload="renderMathInElement(document.querySelector('.markdown-body'))"></script>
+        <script defer src="<?php $this->options->themeUrl('assets/katex/auto.render.min.js'); ?>"></script>
+        <script>
+            window.onload = function() {
+                renderMathInElement(document.querySelector('.markdown-body'), {
+                    delimiters: [{
+                            left: "$$",
+                            right: "$$",
+                            display: true
+                        },
+                        {
+                            left: "$",
+                            right: "$",
+                            display: false
+                        }
+                    ],
+                });
+            };
+        </script>
     <?php endif; ?>
 
 </div>
